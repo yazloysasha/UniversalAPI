c895f118b183cd23bf5deeabbf867b7cdc13d2e2
yazloysasha <yazloysasha@gmail.com>
2024-12-15 20:18:16 +0700

    тест деплоя

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const child_process = require("child_process");
+
+/**
+ * Обновление проекта на сервере
+ */
+async function deploy() {
+  const buildDirectory = `./builds/${Date.now()}-build`;
+  const pm2List = JSON.parse(child_process.execSync("pm2 jlist"));
+
+  child_process.execSync("git pull");
+  child_process.execSync(
+    `tsc --outDir ${buildDirectory} && npx tsconfig-replace-paths -s ./src -o ${buildDirectory}`
+  );
+  child_process.execSync(
+    `pm2 start npm --name "universal-api" -- BUILD_DIRECTORY=${buildDirectory} run start`
+  );
+}
+
+deploy();
