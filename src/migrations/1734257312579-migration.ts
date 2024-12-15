5b97a4473facddb18a5cd74de762795828ba9492
yazloysasha <yazloysasha@gmail.com>
2024-12-15 17:09:14 +0700

    Тест миграций с самого начала

--- src/migrations/1734257312579-migration.ts
+++ src/migrations/1734257312579-migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migration1734256908176 implements MigrationInterface {
-  name = "Migration1734256908176";
+export class Migration1734257312579 implements MigrationInterface {
+  name = "Migration1734257312579";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
